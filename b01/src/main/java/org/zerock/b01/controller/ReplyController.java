8ee167bbfa7188542e121497b6e42e74722351db
dahee oh <ekgml4122@gmail.com>
2023-07-28 20:02:19 +0900

    자바 웹 개발 워크북 6.2 GET 방식으로 특정 게시물의 댓글 목록

--- b01/src/main/java/org/zerock/b01/controller/ReplyController.java
+++ b01/src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -3,14 +3,14 @@ package org.zerock.b01.controller;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.dto.ReplyDTO;
 import org.zerock.b01.service.ReplyService;
 
@@ -40,11 +40,23 @@ public class ReplyController {
         }
 
         Map<String, Long> resultMap = new HashMap<>();
+
         Long rno = replyService.register(replyDTO);
 
         resultMap.put("rno", 111L);
 
         return resultMap;
+    }
+
+    @ApiOperation(value = "Replies of Board", notes = "GET 방식으로 특정 게시물의 댓글 목록")
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno,
+                                             PageRequestDTO pageRequestDTO) {
+
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno,
+                pageRequestDTO);
+
+        return responseDTO;
     }
 
 }
