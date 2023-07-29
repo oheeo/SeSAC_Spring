e803c79c566ea4c25d024959f84e0a66e49276f7
dahee oh <ekgml4122@gmail.com>
2023-07-29 21:42:45 +0900

    자바 웹 개발 워크북 6.3 댓글 시간 처리

--- b01/src/main/java/org/zerock/b01/dto/ReplyDTO.java
+++ b01/src/main/java/org/zerock/b01/dto/ReplyDTO.java
@@ -1,5 +1,7 @@
 package org.zerock.b01.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,7 +27,14 @@ public class ReplyDTO {
 
     @NotEmpty
     private String replyer;
+    
+    // 댓글 등록 시간 JSON 처리 시 포맷팅 지정
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 
-    private LocalDateTime regDate, modDate;
+    private LocalDateTime regDate;
+    
+    // 댓글 수정 시간의 경우 화면에 출력할 일이 없으므로 JSON으로 변환될 때 제외
+    @JsonIgnore
+    private LocalDateTime modDate;
 
 }
