59821a931c7dcac390a81be54d69684a7391dded
dahee oh <ekgml4122@gmail.com>
2023-08-01 23:47:00 +0900

    자바 웹 개발 워크북 7.2 게시물 목록 처리

--- b01/src/test/java/org/zerock/b01/service/BoardServiceTests.java
+++ b01/src/test/java/org/zerock/b01/service/BoardServiceTests.java
@@ -4,11 +4,10 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.zerock.b01.dto.BoardDTO;
-import org.zerock.b01.dto.PageRequestDTO;
-import org.zerock.b01.dto.PageResponseDTO;
+import org.zerock.b01.dto.*;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootTest
@@ -103,6 +102,42 @@ public class BoardServiceTests {
         for (String fileName : boardDTO.getFileNames()) {
             log.info(fileName);
         }  // end for
+    }
+
+    @Test
+    public void testTemoveAll() {
+
+        Long bno = 1L;
+
+        boardService.remove(bno);
+    }
+
+    @Test
+    public void testListWithAll() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResponseDTO<BoardListAllDTO> responseDTO =
+                boardService.listWithAll(pageRequestDTO);
+
+        List<BoardListAllDTO> dtoList = responseDTO.getDtoList();
+
+        dtoList.forEach(boardListAllDTO -> {
+            log.info(boardListAllDTO.getBno()+":"+boardListAllDTO.getTitle());
+
+            if(boardListAllDTO.getBoardImages() != null) {
+                for (BoardImageDTO boardImage : boardListAllDTO.getBoardImages()) {
+                    log.info(boardImage);
+                }
+            }
+
+            log.info("-------------------------------");
+        });
+
+
     }
 
 }
