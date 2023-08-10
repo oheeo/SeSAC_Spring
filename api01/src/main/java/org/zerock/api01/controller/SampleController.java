5928093e64641e023983af42ed4635fcc3b33318
dahee oh <ekgml4122@gmail.com>
2023-08-11 02:44:19 +0900

    자바 웹 개발 워크북 9.1 JWT 인증

--- api01/src/main/java/org/zerock/api01/controller/SampleController.java
+++ api01/src/main/java/org/zerock/api01/controller/SampleController.java
@@ -1,13 +1,14 @@
 package org.zerock.api01.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/sample")
@@ -15,9 +16,15 @@ public class SampleController {
 
     // 단순하게 문자열의 배열을 반환
     // '/api/sample/doA'경로로 호출 가능
-    @ApiOperation("Sample GET doA")
     @GetMapping("/doA")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public List<String> doA() {
         return Arrays.asList("AAA","BBB","CCC");
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/doB")
+    public List<String> doB() {
+        return Arrays.asList("AdminAAA","AdminBBB","AdminCCC");
     }
 }
